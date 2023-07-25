93400225cc3b1e423f4960d4d7f08d85dc5dec97
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-25 20:00:54 +0200

    feat: integration bootstrap

--- projet-final/front-end/src/main.ts
+++ projet-final/front-end/src/main.ts
@@ -2,6 +2,9 @@ import { RequestString, StringDisplay } from './core/custom-types';
 import { Game } from './domains/game';
 import { Player } from './domains/player';
 
+import 'materialize-css/dist/css/materialize.min.css';
+import { TapTarget, Dropdown } from 'materialize-css';
+
 const callBack: StringDisplay = (mess: string) => console.log(mess);
 const requestString: RequestString = (mess: string) => prompt(mess);
 
@@ -22,7 +25,10 @@ game.init();
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tap-target');
-    var instances = MO.TapTarget.init(elems);
+    var instances = TapTarget.init(elems);
 
     instances[0].open();
+
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instancesDropDowns = Dropdown.init(elems, {});
   });
